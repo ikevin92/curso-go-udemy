ebd9c750fb4570760fcbeff4d5a83a89ead3d850
KEVIN ECHEVERRI <ikevin1992@gmail.com>
2023-03-08 20:29:17 -0500

    add figuras modulos

--- 07-modularizacion/main.go
+++ 07-modularizacion/main.go
@@ -1,8 +1,29 @@
 package main
 
-import "paquetes/mensajes"
+import "github.com/donvito/hellomod"
+
+// "paquetes/mensajes"
 
 func main() {
-	mensajes.Hola()
-	mensajes.Imprimir()
+	// mensajes.Hola()
+	// mensajes.Imprimir()
+
+	/*
+		 	cuadrado1 := figuras.Cuadrado{Lado: 10}
+			figuras.Medidas(&cuadrado1)
+
+			circulo1 := figuras.Circulo{Radio: 10}
+			figuras.Medidas(&circulo1)
+	*/
+
+	/*
+		 	p1 := models.Persona{}
+			p1.Constructor("Juan", 20)
+			fmt.Println("Persona:", p1)
+
+			p1.SetNombre("Pedro")
+			fmt.Println("Nombre p1:", p1.GetNombre())
+	*/
+
+	hellomod.SayHello()
 }
